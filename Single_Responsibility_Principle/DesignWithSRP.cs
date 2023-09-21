7175d2b35cc426c680d6e61b2e86e3351a6ca075
Halil Gedik <halilgedik4234@gmail.com>
2023-09-21 13:28:16 +0300

    SRP (Tek Sorumluluk Prensibi)

---
+++ Single_Responsibility_Principle/DesignWithSRP.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_WithSRP;
+
+//veri tabanı işlemleri burada gerçekleştirilir. Tek sorumluluk
+class Database
+{
+    public void Connect()
+    {
+        //...
+    }
+
+    public void DisConnect()
+    {
+        //...
+    }
+}
+
+// user ile ilgili işlemler burada gerçekleştirilir.
+class UserService
+{
+    public List<User> GetUsers()
+    {
+        //........
+        return new List<User>()
+        {
+            new User() {Id=1,Name="Halil"},
+            new User() {Id=1,Name="Ahmet"},
+            new User() {Id=1,Name="Mehmet"},
+        };
+    }
+}
+
+class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    //.......
+}
---
+++ Single_Responsibility_Principle/DesignWithoutSRP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_WithoutSRP;
+
+class Database
+{
+    public void Connect()
+    {
+        //...
+    }
+
+    public void DisConnect()
+    {
+        //...
+    }
+
+    public List<User> GetUsers()
+    {
+        //........
+        return new List<User>()
+        {
+            new User() {Id=1,Name="Halil"},
+            new User() {Id=1,Name="Ahmet"},
+            new User() {Id=1,Name="Mehmet"},
+        };
+    }
+}
+
+class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    //.......
+}
+
+
+
